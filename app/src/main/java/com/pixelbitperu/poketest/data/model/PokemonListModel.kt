bef18b992f408a69609395ab8f6cc9023cad04ef
Oz Medina <oscar.m32@gmail.com>
2022-12-10 07:26:55 -0500

    posible solució

--- app/src/main/java/com/pixelbitperu/poketest/data/model/PokemonListModel.kt
+++ app/src/main/java/com/pixelbitperu/poketest/data/model/PokemonListModel.kt
@@ -1,9 +1,7 @@
 package com.pixelbitperu.poketest.data.model
 
 data class PokemonListModel (
-
     val results: List<PokeResult>
-
     )
 
 data class PokeResult (
--- app/src/main/java/com/pixelbitperu/poketest/ui/launch/LaunchActivity.kt
+++ app/src/main/java/com/pixelbitperu/poketest/ui/launch/LaunchActivity.kt
@@ -3,9 +3,12 @@ package com.pixelbitperu.poketest.ui.launch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
+import com.pixelbitperu.poketest.data.model.PokeResult
 import com.pixelbitperu.poketest.data.model.PokemonListProvider
 import com.pixelbitperu.poketest.databinding.ActivityLaunchBinding
+import com.pixelbitperu.poketest.domain.GetPokemonList
 
 
 class LaunchActivity : AppCompatActivity() {
@@ -32,8 +35,26 @@ class LaunchActivity : AppCompatActivity() {
         binding.btnStart.setOnClickListener {
 //            val intent = Intent(this,PokemonListActivity::class.java)
 //            startActivity(intent)
+            val listaPokemones:List<PokeResult> = emptyList()
+            GetPokemonList().let { listaPokemones}
 
-            println(PokemonListProvider.pokemonListProvider.toString())
+            try{
+                for (i:PokeResult in listaPokemones){
+                    var content:String=""
+                    content+="name: "+i.name+"\n"
+                    content+="url: "+i.url+"\n"
+                    Log.i("pokemones", content.toList().toString())
+//                println(content+"")
+                }
+            }catch (e:Exception){
+                Log.i("pokeERROR", "no listó pipipi")
+
+            }
+
+
+
+
+//                println(PokemonListProvider.pokemonListProvider.toString())
 
 
         }
